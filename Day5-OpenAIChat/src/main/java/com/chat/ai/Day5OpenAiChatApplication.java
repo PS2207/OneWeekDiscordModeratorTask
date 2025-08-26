be561715ed4036aaf7219cbde453955fa6cb5b7c
PS2207 <pragyasinha2207@gmail.com>
2025-08-26 22:32:19 +0530

    uploading day5 project

---
+++ Day5-OpenAIChat/src/main/java/com/chat/ai/Day5OpenAiChatApplication.java
@@ -0,0 +1,18 @@
+package com.chat.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day5OpenAiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day5OpenAiChatApplication.class, args);
+	}
+
+}
+
+
+
+
+
---
+++ Day5-OpenAIChat/src/main/java/com/chat/ai/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.chat.ai.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chat.ai.service.ChatService;
+
+@RestController
+@RequestMapping("/api/v1/chat")
+public class ChatController {
+
+	@Autowired
+	private ChatService chatService;
+	
+	@GetMapping
+	public ResponseEntity<String> generateResponse(@RequestParam(value="inputText") String inputText){
+		String responseText=chatService.generateResponse(inputText);
+		return ResponseEntity.ok(responseText);
+	}
+	
+	
+}
